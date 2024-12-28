fe7507418617905e1a77bbc7c445864c7edc14c6
Hassan Mehmood <mehassanhmood@gmail.com>
2024-12-28 02:59:07 -0500

    Authentication half way done

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,10 @@
 "use server"
 
+import { ID } from "node-appwrite"
+import { createAdminClient, createSessionClient } from "../appwrite"
+import { cookies } from "next/headers"
+import { parseStringify } from "../utils"
+
 export const signIn = async () => {
     try {
         //Mutations or database modifications or making a fetch/get request.
@@ -11,9 +16,38 @@ export const signIn = async () => {
 }
 
 export const signUp = async (userData: SignUpParams) => {
-    try {
 
+    const {email, password, firstName, lastName} = userData;
+    try {
+        const { account } = await createAdminClient();
+
+        const newUserAccount = await account.create(
+            ID.unique(), 
+            email, 
+            password, 
+            `${firstName} ${lastName}`
+        );
+
+        const session = await account.createEmailPasswordSession(email, password);
+      
+        await cookies().set("appwrite-session", session.secret, {
+          path: "/",
+          httpOnly: true,
+          sameSite: "strict",
+          secure: true,
+        });
+        return parseStringify(newUserAccount);
     } catch (error) {
         console.log(error)
     }
 }
+
+export async function getLoggedInUser() {
+    try {
+      const { account } = await createSessionClient();
+      return await account.get();
+    } catch (error) {
+      return null;
+    }
+  }
+  
